40cd3823aec64b9b282832b3e93baf714556308f
Adam Paiz <paiza@oregonstate.edu>
2018-01-20 17:54:15 -0600

    removed stuff in switch statement that was commented out for adventurer

--- projects/paiza/dominion/dominion.c
+++ projects/paiza/dominion/dominion.c
@@ -687,26 +687,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      /*
-      while(drawntreasure<2){
-        if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-          shuffle(currentPlayer, state);
-        }
-        drawCard(currentPlayer, state);
-        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-        drawntreasure++;
-        else{
-          temphand[z]=cardDrawn;
-          state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-          z++;
-        }
-      }
-      while(z-1>=0){
-        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-        z=z-1;
-      }
-      */
       playAdventurer(state, currentPlayer, &z, & cardDrawn, & drawntreasure, temphand);
       return 0;
 
