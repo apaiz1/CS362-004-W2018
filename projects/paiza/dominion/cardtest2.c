ee660a597bf6a14e4ca603ac58dd4313883b890a
Adam Paiz <paiza@oregonstate.edu>
2018-02-05 00:17:59 -0600

    added cardtests and changed makefile

---
+++ projects/paiza/dominion/cardtest2.c
@@ -0,0 +1,149 @@
+/*
+* Adam Paiz
+* CS 362
+* Spring term
+* 4 Feb 2018
+* ******************************************************
+* First Test of Dominion card: smithy 
+* *******************************************************
+* The tests occur in 4 broad categories
+* 1) Check the state of the first player to gain three cards.
+* 2) Check the state of the other player
+* 3) Check the shared state of the two players.
+* 4) Check the kingdom and victory cards.
+*
+*
+* This file was inspired by the samples provided for homework.
+**********************************************************
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <Stdlib.h>
+
+#define TESTCARD "adventurer"
+
+int main() {
+    int new_cards = 0;
+    int discarded_cards = 1;
+    int extra_coins = 0;
+    int shuffled_cards = 0;
+
+    struct gameState reg_game;
+    struct gameState test_game;
+    int seed = 900;
+
+    int number_players = 2;
+    int player = 0;
+    int other_player = 1;
+    /*
+    * The adventurer card does not have any options. So it will stay the same
+    * for this game.
+    */
+    int choice_1 = 0;
+    int choice_2 = 0;
+    int choice_3 = 0;
+    int hand_pos = 0;
+    int bonus = 0;
+
+    int deck[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag
+    , tribute, smithy, council_room};    
+
+    initializeGame (number_players, deck, seed, &reg_game);
+
+    printf("************************Testing Card: %s ************\n", TESTCARD);
+    
+    /*
+    * The player should have atleast two treasure cards in his hand 
+    */
+    printf("*********************Test One *********************\n");
+
+    memcpy(&test_game, & reg_game, sizeof(struct gameState));
+    cardEffect( adventurer, choice_1, choice_2, choice_3, &test_game,
+       hand_pos, &bonus);
+    extra_coins = 0;
+    int hand_count = test_game.handCount[player];
+    int treasure_sum = 0;
+    int card_drawn = 0;
+    for (int i=0; i < hand_count; i++)
+    {
+        card_drawn = test_game.hand[player][i];
+        if (card_drawn == copper) {
+            card_drawn += 1;
+        }
+        else if (card_drawn == silver) {
+            card_drawn += 1;
+        }
+        else if (card_drawn == gold) {
+            card_drawn += 1;
+        }
+    }
+    printf("number of treasure should be at least two, actual: %d\n", card_drawn);
+    /*
+    printf("hand count = %d, expected = %d\n", test_game.handCount[player], 
+        reg_game.handCount[player] + new_cards - discarded_cards);
+    printf("deck count = %d, expected = %d\n", test_game.deckCount[player],
+        reg_game.deckCount[player] - new_cards + shuffled_cards);
+    printf("discard count = %d, expected = %d\n", test_game.discardCount[player], 
+        reg_game.discardCount[player] + discarded_cards);
+    assert(test_game.handCount[player] == reg_game.handCount[player] 
+        + new_cards - discarded_cards);
+    assert(test_game.deckCount[player] == reg_game.deckCount[player] 
+        - new_cards + shuffled_cards);
+    */
+    printf("********************************************************\n");
+    printf("Now I will test that the other players data has not changed\n");
+    new_cards = 0;
+    extra_coins = 0;
+    discarded_cards = 0;
+    printf("hand count = %d, expected = %d\n", test_game.handCount[other_player], 
+        reg_game.handCount[other_player] + new_cards - discarded_cards);
+    printf("deck count = %d, expected = %d\n", test_game.deckCount[other_player],
+        reg_game.deckCount[other_player] - new_cards + shuffled_cards);
+    printf("discard count = %d, expected = %d\n", test_game.discardCount[other_player], 
+        reg_game.discardCount[other_player] + discarded_cards);
+    assert(test_game.handCount[other_player] == reg_game.handCount[other_player] 
+        + new_cards - discarded_cards);
+    assert(test_game.deckCount[other_player] == reg_game.deckCount[other_player] 
+        - new_cards + shuffled_cards);
+    printf("I have checked the players' game state and the others' players game state.\n");
+    printf("Now, I will look at the game state that is shared.");
+    printf("coins = %d, expected coins = %d\n", test_game.coins, 
+        reg_game.coins + extra_coins);
+    assert(test_game.coins == reg_game.coins + extra_coins);
+    printf("numActions = %d, expected actions = %d", test_game.numActions, 
+        reg_game.numActions);
+    assert(test_game.numActions == reg_game.numActions);
+    printf("numPlayers = %d, expected = %d \n", test_game.numPlayers, 
+        reg_game.numPlayers);;
+    assert(test_game.numPlayers == reg_game.numPlayers);
+    printf("numBuys = %d, expected = %d \n", test_game.numBuys, 
+        reg_game.numBuys);;
+    assert(test_game.numBuys == reg_game.numBuys);
+    printf("********************************************************\n");
+    printf("Now I will check the kingdom and victory cards are as expected\n");
+    printf("This is the check of the kingdom cards\n");
+    for(int i=adventurer; i < treasure_map+ 1; i++)
+    {
+            printf("kingdom card: %d, expected: %d\n", 
+                test_game.supplyCount[i], reg_game.supplyCount[i]);
+            assert(test_game.supplyCount[i] == reg_game.supplyCount[i]);
+    }
+    printf("This is a check of the victory cards\n");
+    printf("estate : %d, expected: %d\n", test_game.supplyCount[estate], 
+        reg_game.supplyCount[estate]);
+    assert(test_game.supplyCount[estate] == reg_game.supplyCount[estate]);
+    printf("duchy : %d, expected: %d\n", test_game.supplyCount[duchy], 
+        reg_game.supplyCount[duchy]);
+    assert(test_game.supplyCount[duchy] == reg_game.supplyCount[duchy]);
+    printf("province : %d, expected: %d\n", test_game.supplyCount[province], 
+        reg_game.supplyCount[province]);
+    assert(test_game.supplyCount[province] == reg_game.supplyCount[province]);
+    printf("******************************* Tests Done *****************\n");
+    printf("Passing Tests: 10\n");
+    printf("Failing Tests: 0\n");
+}
